64bf53dbbba8d9dc27a163056d425e7b427e41ce
Szczurowsky <kamilszczurowski@hotmail.com>
2024-07-27 17:00:26 +0200

    fix: add proper handling for name function

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,23 +9,28 @@ version = "1.0.1"
 repositories {
     mavenCentral()
     maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
         url = uri("https://repo.szczurowsky.pl/releases")
     }
     maven {
         name = "CodeMC"
         url = uri("https://repo.codemc.io/repository/maven-public/")
     }
+    mavenLocal()
 }
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("de.tr7zw:item-nbt-api:2.12.4")
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:24.1.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    testImplementation("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.68.0")
 }
 
 
